a42e724cef05013df30bd739c9856f13c9447ea3
tengattack <tengattack@foxmail.com>
2016-06-12 23:46:47 +0800

    add event emitter

--- aeroplane.js
+++ aeroplane.js
@@ -6,6 +6,19 @@ var config = require('./config');
 var BotApi = require('./lib/botapi');
 
 var ba = new BotApi(config.token, {
-  proxyUrl: config.proxy
+  proxyUrl: config.proxy,
+  botName: config.bot_name,
 });
+
+// define command
+ba.commands.on('flee', (upd, followString) => {
+  console.log(upd, followString);
+  /*this.sendMessage({
+    chat_id: chat_id,
+    text: '_yk sb_'
+  }, function (err, result) {
+    console.log(err, result);
+  });*/
+});
+
 ba.start();
--- config.example.js
+++ config.example.js
@@ -3,6 +3,7 @@ var config = {
   "update_type": "long-polling", // or "webhook"
   "webhook": "https://www.example.com/<token>",
   //"proxy": "http://127.0.0.1:8118",
+  "bot_name": "aeroplanechess_bot", // check bot name
 };
 
 module.exports = config;
