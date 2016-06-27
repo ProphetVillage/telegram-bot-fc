7159b49678fd88fd9ef81a3b95bbd8eaf4f01ae7
tengattack <tengattack@foxmail.com>
2016-06-27 17:18:11 +0800

    add Chess play

--- chess.js
+++ chess.js
@@ -6,6 +6,10 @@ const Chess = require('./lib/chess');
 
 var db = new DB(config.db);
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 setTimeout(() => {
 var chess = new Chess(db);
 var chat_id = 0;
@@ -19,11 +23,49 @@ chess.load(chat_id, (err) => {
   if (chess.start()) {
     console.log(chess.playerlist);
     console.log(chess.chessboard);
-    chess.takeoff();
-    console.log(chess.chessboard);
-    chess.next(0, 4);
-    console.log(chess.chessboard);
-    chess.moveNext();
+    
+    
+    while (true) {
+      var num = getRandomInt(1, 7);
+      var takeoff = false;
+      if (num === 6) {
+        if (chess.takeoff()) {
+          takeoff = true;
+        }
+      } 
+      if (!takeoff) {
+        var choice = 0;
+        var nomove = true;
+        while (choice < 4) {
+          if (chess.next(choice, num)) {
+            nomove = false;
+            break;
+          } else {
+            choice++;
+          }
+        }
+        if (nomove) {
+          console.log(chess.currentPlayer().chessman, 
+            num, choice, 'nomove');
+        } else {
+          console.log(chess.currentPlayer().chessman, 
+            num, choice, chess.chessboard);
+        }
+      } else {
+        console.log(chess.currentPlayer().chessman, 
+          'takeoff', chess.chessboard);
+      }
+      
+      var win = chess.checkWin();
+      //console.log('checkWin', win);
+      if (win && chess.isEnded()) {
+        console.log('win! and end');
+        break;
+      } else {
+        chess.moveNext();
+      }
+    }
+    
     chess.save((err, r) => {
       if (err) {
         console.log(err);
